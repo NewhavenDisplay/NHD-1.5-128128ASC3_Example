fb019a7cd14677fde59f5c3910e3dd84a462f384
Paul <Pbartek@users.noreply.github.com>
2015-09-08 08:32:08 -0500

    Update test.ino

--- examples/test/test.ino
+++ examples/test/test.ino
@@ -3,7 +3,7 @@
 
 test.ino
 
-Program for writing to Newhaven Display 128x128 Graphic Color OLED with SSD1351 controller.
+Program for writing to Newhaven Display's 128x128 Graphic Color OLED with SSD1351 controller.
 
  Pick one up today in the Newhaven Display shop!
  ------> Coming Soon!
