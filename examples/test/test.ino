0efc3e7414718119df7328f6dbf1d0e8b7959067
Paul <Pbartek@users.noreply.github.com>
2016-01-25 09:15:59 -0600

    Update test.ino

--- examples/test/test.ino
+++ examples/test/test.ino
@@ -6,7 +6,7 @@ test.ino
 Program for writing to Newhaven Display's 128x128 Graphic Color OLED with SSD1351 controller.
 
  Pick one up today in the Newhaven Display shop!
- ------> Coming Soon!
+ ------> http://www.newhavendisplay.com/nhd15128128asc3-p-9287.html
 
 This code is written for the Arduino Uno R3.
 
